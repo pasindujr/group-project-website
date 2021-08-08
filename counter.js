b76c5fec77abf206d671d03f8db28143d3fe6c6b
pasindujr <pasindujayanada@gmail.com>
2021-08-08 17:48:46 +0530

    visitor count updated

---
+++ counter.js
@@ -0,0 +1,11 @@
+const countElement = document.getElementById("count");
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch("https://api.countapi.xyz/update/atikegalle/group/?amount=1")
+		.then((res) => res.json())
+		.then((res) => {
+			countElement.innerHTML = res.value;
+		});
+}
