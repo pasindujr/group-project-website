25f412ded18820d06a38ce88a738d76b3d6c0fde
pasindujr <pasindujayanada@gmail.com>
2021-08-19 22:22:17 +0530

    Profile edit function added

--- counter.js
+++ counter.js
@@ -3,7 +3,7 @@ const countElement = document.getElementById("count");
 updateVisitCount();
 
 function updateVisitCount() {
-	fetch("https://api.countapi.xyz/update/atikegalle/group/?amount=1")
+	fetch("https://api.countapi.xyz/update/atikegalle/group/?amount=+1")
 		.then((res) => res.json())
 		.then((res) => {
 			countElement.innerHTML = res.value;
