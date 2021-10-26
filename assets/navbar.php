fb20ba8f3a19f2d72ed030014691369fd062ee42
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-10-26 22:06:36 +0530

    hyperlinks fixed

--- assets/navbar.php
+++ assets/navbar.php
@@ -3,7 +3,7 @@
   <div class="container-fluid"> 
     
     <!-- Navbar brand --> 
-    <a class="navbar-brand" href="index.html"><img src="assets/logo.png" width="30" height="30" alt=""></a> 
+    <a class="navbar-brand" href="index.php"><img src="assets/logo.png" width="30" height="30" alt=""></a> 
     
     <!-- Toggle button -->
     <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
@@ -13,19 +13,19 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         
         <!-- Link -->
-        <li class="nav-item"> <a class="nav-link" href="announcement.html">Announcements</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="gallery.html">Gallery</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="contact.html">Contact Us</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="member-enter.html">
+        <li class="nav-item"> <a class="nav-link" href="announcement.php">Announcements</a> </li>
+        <li class="nav-item"> <a class="nav-link" href="gallery.php">Gallery</a> </li>
+        <li class="nav-item"> <a class="nav-link" href="contact.php">Contact Us</a> </li>
+        <li class="nav-item"> <a class="nav-link" href="member-enter.php">
           <text style="color: aqua;"><strong>Enter ATI</strong> </text>
           </a> </li>
         <!-- Login Dropdown -->
         <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false"> <i class="fa fa-fw fa-user"></i> </a> 
           <!-- Dropdown menu -->
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li> <a class="dropdown-item" href="profile.html">Your Profile</a> </li>
-            <li> <a class="dropdown-item" href="register.html">Register</a> </li>
-            <li> <a class="dropdown-item" href="login.html">Login</a> </li>
+            <li> <a class="dropdown-item" href="member-profile.php">Your Profile</a> </li>
+            <li> <a class="dropdown-item" href="member-register.php">Register</a> </li>
+            <li> <a class="dropdown-item" href="member-login-profile.php">Login</a> </li>
           </ul>
         </li>
       </ul>
--- forget-password.php
+++ forget-password.php
@@ -35,43 +35,9 @@ function navigate( $location = null, $msg = null ) {
 <body>
 <div id="snackbar">Incorrect Username/Password Plese recheck and enter again</div>
 <script type="text/javascript" src="snackbar.js"></script> 
+
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark mb-3" style="background-color:#03045e;"> 
-  <!-- Container wrapper -->
-  <div class="container-fluid"> 
-    
-    <!-- Navbar brand --> 
-    <a class="navbar-brand" href="index.html"><img src="assets/logo.png" width="30" height="30" alt=""></a> 
-    
-    <!-- Toggle button -->
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
-    
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-        
-        <!-- Link -->
-        <li class="nav-item"> <a class="nav-link" href="announcement.html">Announcements</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="gallery.html">Gallery</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="contact.html">Contact Us</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="member-enter.html">
-          <text style="color: aqua;"><strong>Enter ATI</strong> </text>
-          </a> </li>
-        
-        <!-- Login Dropdown -->
-        <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false"> <i class="fa fa-fw fa-user" ></i> </a> 
-          <!-- Dropdown menu -->
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li> <a class="dropdown-item" href="profile.html">Your Profile</a> </li>
-            <li> <a class="dropdown-item" href="register.html">Register</a> </li>
-            <li> <a class="dropdown-item" href="login.html">Login</a> </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <!-- Container wrapper --> 
-</nav>
+<?php require_once "assets/navbar.php"; ?>
 <!-- Navbar --> 
 
 <!-- Content -->
